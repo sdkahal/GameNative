a6ab190e18eeb35a810219a269d9303f3803f3b9
Almond <88301593+sdkahal@users.noreply.github.com>
2026-04-27 03:10:57 +0800

    Update LibraryAppScreen.kt

--- app/src/main/java/app/gamenative/ui/screen/library/LibraryAppScreen.kt
+++ app/src/main/java/app/gamenative/ui/screen/library/LibraryAppScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
@@ -34,8 +33,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.relocation.BringIntoViewRequester
