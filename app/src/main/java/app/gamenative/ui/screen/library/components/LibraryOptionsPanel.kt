97411e73f2416ee507f70ed9d7d57028c64cda55
Almond <88301593+sdkahal@users.noreply.github.com>
2026-04-27 17:43:42 +0800

    revert

--- app/src/main/java/app/gamenative/ui/screen/library/components/LibraryOptionsPanel.kt
+++ app/src/main/java/app/gamenative/ui/screen/library/components/LibraryOptionsPanel.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -135,7 +136,8 @@ fun LibraryOptionsPanel(
             ) {
                 Column(
                     modifier = Modifier
-                        .fillMaxSize(),
+                        .fillMaxSize()
+                        .statusBarsPadding()
                 ) {
                     Row(
                         modifier = Modifier
